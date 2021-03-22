8c19813b0d6835a7326899655f79a22c766b22e8
gustavo-exe <gus.elim2@gmail.com>
2021-03-22 17:03:27 -0600

    Pie de pagina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import PieChart from "./components/Charts/PieChart";
-
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
       <div className="App">
         <NavBar/>
         <PieChart/>
+        <Footer/>
       </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+function Footer() {
+    return(
+        <div className="Footer" >
+            <p>
+            COVID-19:  ESTADITICAS DE SOBREVICENCIA (ECHO EN HN) EXPIRA 00-00-00
+            </p>
+        </div>
+    )
+}
+
+export default Footer;
